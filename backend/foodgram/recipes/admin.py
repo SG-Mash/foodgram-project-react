e616cec66d9c2d7325a5ee1d3a3e4546493b0db1
Сергей Грибанов <mashrumoff@googlemail.com>
2023-04-18 09:39:28 +0300

    Recipes models

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,82 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes.models import Tag, Ingredient, Recipe, RecipeIngredient, Favorite, ShoppingCart
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'color',
+        'slug'
+    )
+    search_fields = ('name', 'color', 'slug')
+    list_filter = ('name', 'color', 'slug')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'measurement_unit'
+    )
+    search_fields = ('name', )
+    list_filter = ('name', )
+    empty_value_display = '-пусто-'
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'author',
+        'favorite_amount'
+    )
+    search_fields = ('name', 'author')
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = '-пусто-'
+    inlines = [RecipeIngredientInline, ]
+
+    def favorite_amount(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'recipe',
+        'ingredient',
+        'amount'
+    )
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'recipe'
+    )
+    search_fields = ('user', 'recipe')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'recipe'
+    )
+    search_fields = ('user', 'recipe')
+    empty_value_display = '-пусто-'
