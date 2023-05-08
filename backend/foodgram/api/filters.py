1f45b150bcfc52c81816d6c5297a43f218cf409a
Сергей Грибанов <mashrumoff@googlemail.com>
2023-05-08 18:49:32 +0300

    workflow test 33

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,13 +1,18 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe, Tag
+from rest_framework import SearchFilter
+# from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Recipe
+from users.models import User
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.ModelMultipleChoiceFilter(
-        queryset=Tag.objects.all(),
-        field_name='tags__slug',
-        to_field_name='slug',
-    )
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    # tags = filters.ModelMultipleChoiceFilter(
+    #     queryset=Tag.objects.all(),
+    #     field_name='tags__slug',
+    #     to_field_name='slug',
+    # )
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
@@ -28,9 +33,12 @@ class RecipeFilter(FilterSet):
         return queryset
 
 
-class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='istartwith')
+class IngredientSearchFilter(SearchFilter):
+    search_param = 'name'
 
-    class Meta:
-        model = Ingredient
-        fields = ('name', )
+# class IngredientFilter(FilterSet):
+#     name = filters.CharFilter(lookup_expr='istartwith')
+#
+#     class Meta:
+#         model = Ingredient
+#         fields = ('name', )
