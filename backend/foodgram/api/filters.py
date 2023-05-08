422857e570f00628cb142782bd519eea6faa6045
Сергей Грибанов <mashrumoff@googlemail.com>
2023-05-08 19:01:53 +0300

    workflow test 35

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,18 +1,18 @@
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework import SearchFilter
 # from recipes.models import Ingredient, Recipe, Tag
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 from users.models import User
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    # tags = filters.ModelMultipleChoiceFilter(
-    #     queryset=Tag.objects.all(),
-    #     field_name='tags__slug',
-    #     to_field_name='slug',
-    # )
+    # tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    # author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug',
+    )
     is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='get_is_in_shopping_cart'
