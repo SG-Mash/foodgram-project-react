66007f1f7c190b9036d17d801e140d376c830205
Сергей Грибанов <mashrumoff@googlemail.com>
2023-05-08 19:06:38 +0300

    workflow test 36

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -2,7 +2,6 @@ from django_filters.rest_framework import FilterSet, filters
 from rest_framework import SearchFilter
 # from recipes.models import Ingredient, Recipe, Tag
 from recipes.models import Recipe, Tag
-from users.models import User
 
 
 class RecipeFilter(FilterSet):
@@ -24,7 +23,7 @@ class RecipeFilter(FilterSet):
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
-            return queryset.filter(fovorites__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def get_is_in_shopping_cart(self, queryset, name, value):
