071f7d9472c8903bfddbfb28be836f7eccb9a929
Сергей Грибанов <mashrumoff@googlemail.com>
2023-05-08 14:57:28 +0300

    workflow test 31

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -100,7 +100,7 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipe_ingredients',
+        related_name='recipe',
         verbose_name='Рецепт'
     )
     ingredient = models.ForeignKey(
